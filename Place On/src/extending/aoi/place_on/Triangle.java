d67dc333a176686b51a3dcabf48dac535c340684
TimothyFish <timothy@timothyfish.com>
2019-04-17 03:24:58 -0500

    Updated license and copyright information for the repository.

--- Place On/src/extending/aoi/place_on/Triangle.java
+++ Place On/src/extending/aoi/place_on/Triangle.java
@@ -1,4 +1,22 @@
 /**
+    Place On Plugin from Chapter 5 of the book "Extending Art of Illusion: Scripting 3D Scene Creation"
+    Copyright (C) 2019, 2011  Timothy Fish
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+/**
  * A simple class to hold the points of a triangle and to 
  * compute their normal.
  */
